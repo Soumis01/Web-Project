1feb6a8f0a14957cccca719350d236d1353c6b22
Maite Natera <109771419+Soumis01@users.noreply.github.com>
2022-08-11 18:06:34 -0400

    Delete ejercicio5.py

--- ejercicio5.py
+++
@@ -1,6 +0,0 @@
-#preguanta las horas y el coste de cada hora luego multiplica estos para dar con el resultado
-horas = float(input('Introduce las horas que trabajas: '))
-coste = float(input('Introduce lo que cobras por hora: '))
-paga = horas * coste
-
-print('Tu paga es', paga)
