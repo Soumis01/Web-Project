146512628f05750b893fd7d974422e4fdf960504
Maite Natera <109771419+Soumis01@users.noreply.github.com>
2022-08-11 18:00:28 -0400

    Add files via upload
     Un programa que pregunte al usuario por el número de horas trabajadas y el coste por hora. Después debe mostrar por pantalla la paga que le corresponde.

---
+++ ejercicio5.py
@@ -0,0 +1,6 @@
+#preguanta las horas y el coste de cada hora luego multiplica estos para dar con el resultado
+horas = float(input('Introduce las horas que trabajas: '))
+coste = float(input('Introduce lo que cobras por hora: '))
+paga = horas * coste
+
+print('Tu paga es', paga)
