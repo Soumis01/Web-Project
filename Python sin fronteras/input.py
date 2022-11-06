d29fbd6531a8ee9c457b7dc5a20115f79474f1f0
Maite Natera <109771419+Soumis01@users.noreply.github.com>
2022-11-06 15:13:05 -0500

    Update

---
+++ Python sin fronteras/input.py
@@ -0,0 +1,10 @@
+dato = input('Ingrese dato: ')
+
+lista = ['hola', 'mundo' 'jade' 'feliz' 'adios']
+
+if lista.count(dato) > 0:
+    print('El dato existe: ', dato)
+else:
+    print('El dato no existe: ', dato)
+
+    
